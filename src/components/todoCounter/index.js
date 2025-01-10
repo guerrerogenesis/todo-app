e5beba6579a24f3d9731c3ea658bc42956580831
guerrerogenesis <guerrerogenesisd@gmail.com>
2025-01-10 13:21:17 -0600

    fix margin

--- src/components/todoCounter/index.js
+++ src/components/todoCounter/index.js
@@ -7,7 +7,7 @@ function TodoCounter({total, completed}){
 
     return (
         <div className="h-1/5">
-            <div className="w-5/6 flex justify-center m-auto mt-28">
+            <div className="w-5/6 flex justify-center m-auto mt-40">
             </div>
             <h3 className="mt-10 text-2xl md:text-2xl lg:text-3xl text-center text-white font-quicksand">Has completado {completed}/{total} Tareas</h3>
         </div>
